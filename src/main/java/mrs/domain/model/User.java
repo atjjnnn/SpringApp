d23e8c629a9e9ad64536ba0bfe387b50788d7a9a
atjjn <atsuki.0908@icloud.com>
2024-03-14 16:30:24 +0900

    Initial commit

---
+++ src/main/java/mrs/domain/model/User.java
@@ -0,0 +1,48 @@
+package mrs.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usr")
+public class User {
+	@Id
+	private String userId;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@Enumerated(EnumType.STRING)
+	private RoleName rolename;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
